9995e2b1542312be3d9761ea44a365a472297ee3
ViiGamesCompany <Farzadi@users.noreply.github.com>
2020-04-28 15:51:29 +0430

    3:51 PM 4/28/2020

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private PlayerController1 playerController;
+    private PlayerController2 playerController2;
+    private Vector3 playerPosition;
+    private float distanceToMoveX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = FindObjectOfType<PlayerController1>();
+        playerController2 = FindObjectOfType<PlayerController2>();
+        playerPosition = playerController.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToMoveX = playerController.transform.position.x - playerPosition.x;
+
+        transform.position = new Vector3(transform.position.x + distanceToMoveX, transform.position.y , transform.position.z);
+        playerPosition = playerController.transform.position;
+    }
+}
---
+++ Assets/Scripts/Character/PlayerController2.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+
+    public float runSpeed, jumpValue;
+    public LayerMask groundLayers;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+
+
+    private float middleOfScreen;
+    private Rigidbody2D myRigid;
+    private bool isOnGround;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        middleOfScreen = Screen.width / 2;
+        myRigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check if player on ground
+        isOnGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayers);
+
+        //move player
+        myRigid.velocity = new Vector2(runSpeed, myRigid.velocity.y);
+
+        //check touch position and jump
+        if (Input.touchCount > 0)
+        {
+            foreach (Touch touch in Input.touches)
+            {
+                if (touch.phase == TouchPhase.Began)
+                {
+                    if (touch.position.x > middleOfScreen)
+                    {
+                        if (isOnGround)
+                        {
+                            myRigid.velocity = new Vector2(myRigid.velocity.x, jumpValue);
+                        }
+                    }
+                }
+            }
+        }
+        //jump for test
+        if (Input.GetKeyDown(KeyCode.C) == true)
+        {
+            if (isOnGround)
+            {
+                myRigid.velocity = new Vector2(myRigid.velocity.x, jumpValue);
+            }
+
+        }
+    }
+}
